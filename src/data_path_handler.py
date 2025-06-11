97b6db3cf2659a52fdcbd9417f88ec0b999001e1
Marek <marek.j.slipski@jpl.nasa.gov>
2025-06-11 09:58:53 -0700

    clean up readers. start hp fit in nb

--- src/data_path_handler.py
+++ src/data_path_handler.py
@@ -1,38 +1,93 @@
+from bs4 import BeautifulSoup
 import datetime as dt
-from typing import Union
+import requests
+from typing import Union, List
 from util.log import logger
 
 
-class FilenameBuilder():
+def choose_formatter(pds):
+    if pds:
+        return PDSFileFormatter()
+    else:
+        raise NotImplementedError
+    
 
+class NGIMSFilename():
+    """
+    Class to build NGIMS filenames or strip info from filenames
+    """
     utc_str_format = "%Y%m%dT%H%M%S"
+    prefix = "mvn_ngi"
 
-    def build_filename(self, tid: int, utc: Union[dt.datetime, str], version: int, revison: int):
-        if type(utc) != str:
-            utc = utc.strftime(self.utc_str_format)
-        return utc
+    def __init__(self, filename):
+        self.filename = filename
+        self.parse_filename(filename)
+            
+    def parse_filename(self, filename):
+        parts = filename.split("_")
+        prefix = f"{parts[0]}_{parts[1]}"
+        if prefix != self.prefix:
+            raise ValueError(f"Expected prefix {self.prefix} but found {prefix}")
+        file_label_tid = parts[3].split("-")
+        self.level = parts[2]
+        self.tid = int(file_label_tid[-1])
+        self.file_label = parts[3].replace(f"-{self.tid}", "")
+        self.suffix = parts[-1].split(".")[-1]
+        self.revision = parts[-1].split(".")[0].split("r")[-1]
+        self.version = parts[-2].split("v")[-1]
+        self.utc = dt.datetime.strptime(parts[4], self.utc_str_format)
+    
+    @classmethod
+    def build_filename(
+        cls,
+        level:str=None, 
+        file_label:str=None, 
+        tid:int=None, 
+        utc:Union[dt.datetime, str]=None, 
+        version:int=None, 
+        revision:int=None, 
+        suffix:str=None
+    ):
+        if isinstance(utc, dt.datetime):
+            utc_str = utc.strftime(cls.utc_str_format)
+        else:
+            utc_str = utc
+        return cls(f"{cls.prefix}_{level}_{file_label}-{tid}_{utc_str}_v{str(version).zfill(2)}_r{str(revision).zfill(2)}.{suffix}")
+
+    
 
 class PDSFileFormatter():
 
     url_base = "https://atmos.nmsu.edu/PDS/data/PDS4/MAVEN/ngims_bundle/l2"
 
-    def __init__(self, level: str, filetype: str) -> None:
-        #self._check_valid_level(level)
-        self.level = level # L1, L2, etc.
-        self.filetype = filetype
-        #self.data_record = self.level_record_map[level]
-        #logger.info("Setup to load L2 files from PDS")
+    def __init__(self):
+        pass
 
-    #def _check_valid_level(self, level: str) -> None:
-        #if level not in self.level_record_map.keys():
-        #    raise ValueError(
-        #        f"Level {level} not recognized. "
-        #        f"Expected one of {self.level_record_map.keys()}"
-        #    )
-
-    
+    def find_files_within_date_range(self, start_date: dt.datetime, end_date: dt.datetime, file_label: str) -> List[str]:
+        all_files = []
+        for year in range(start_date.year, end_date.year+1):
+            if year == start_date.year:
+                year_month_start = start_date.month
+            else:
+                year_month_start = 1
+            if year == end_date.year:
+                year_month_end = end_date.month
+            else:
+                year_month_end = 12
+            for month in range(year_month_start, year_month_end+1):
+                url = self.make_month_url(year, month)
+                file_links = self.find_all_files_in_directory(url)
+                ngi_filenames = [NGIMSFilename(f) for f in file_links]
+                month_paths = [f"{url}/{nf.filename}" for nf in ngi_filenames if (nf.utc >= start_date and nf.utc<end_date) and (nf.file_label==file_label)]
+                all_files += month_paths
+        return all_files
+            
+    def find_all_files_in_directory(self, url):
+        response = requests.get(url)
+        status = response.status_code
+        soup = BeautifulSoup(response.content, 'html.parser')
+        file_links = [l for l in [link.get('href') for link in soup.find_all('a', href=True)] for ft in ["csv"] if ft in l]
+        return file_links
 
     def make_month_url(self, year, month):
         return f"{self.url_base}/{year}/{str(month).zfill(2)}/"
-
-    #def build_url(self, ):
--- src/reader.py
+++ src/reader.py
@@ -1,4 +1,8 @@
+import datetime as dt
 import pandas as pd
+from typing import List, Union, Tuple
+
+from data_path_handler import choose_formatter
 
 class NGIMSReader():
     """
@@ -7,10 +11,31 @@ class NGIMSReader():
     def __init__(self, pds=True):
         self.pds = pds
 
-    def read(self, filename, level="L2", filetype="csn", **kwargs):
-        if level=="L2" and filetype=="csn":
-            na_values = [-9999, "", "  ", "-999"]  # values to treat as NaN
-            df = pd.read_csv(filename,na_values=na_values, **kwargs).drop_duplicates()
+    def read(self, filename: str, level="L2", file_label="csn-abund", **kwargs):
+        if level=="L2" and file_label=="csn-abund":
+            na_values = [-9999, "", "  ", "-999", -999]  # values to treat as NaN
+            df = pd.read_csv(filename,na_values=na_values, parse_dates=["t_utc"], **kwargs).drop_duplicates()
         return df
 
-#class NGIMSLoader():
+class NGIMSLoader():
+    def __init__(self, pds=True) -> None:
+        self.pds = pds
+        self.reader = NGIMSReader(self.pds)
+        self.file_formatter = choose_formatter(pds=self.pds)
+
+    def load(
+            self,
+            files: Union[List[str], str, None]=None, 
+            date_range: Union[Tuple[dt.datetime, dt.datetime], None]=None,
+            file_label = "csn-abund",
+            **kwargs
+        ):
+        if isinstance(files, str):
+            files = [files]
+        elif files is None and date_range is not None:
+            files = self.file_formatter.find_files_within_date_range(*date_range, file_label=file_label)
+        else:
+            NotImplementedError()
+        dfs = [self.reader.read(f, **kwargs) for f in files]  # load all files
+        df = pd.concat(dfs)  # combine
+        return df
